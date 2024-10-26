3849f7f40a31fb02acf4dec9a3c73ef9ddf479a1
Babayev2326254 <f.babayev1389@gmail.com>
2024-10-26 15:33:30 +0400

    primitiv

---
+++ src/main/java/az/edu/turing/fxbabayev.java
@@ -0,0 +1,29 @@
+package az.edu.turing;
+public class fxbabayev {
+
+    public static void main(String[] args) {
+        byte bytValue = 19 ;
+        short shotValue = 9877;
+        int intValue = 88889900;
+        long longValue = 7899999L;
+        float floatValue = 89.88f ;
+        double doubleValue = 99.55 ;
+        boolean boolValue = false ;
+        char charValue = 'F' ;
+
+
+
+
+        System.out.print(bytValue);
+        System.out.print(" "+shotValue);
+        System.out.println(" " + intValue);
+        System.out.print(longValue);
+        System.out.printf("float  %s" , floatValue);
+        System.out.printf("double  %s" , doubleValue);
+        System.out.print("   kafeye gedek Vusal ?" + boolValue);
+        System.out.print(" " + charValue);
+
+
+
+    }
+}
