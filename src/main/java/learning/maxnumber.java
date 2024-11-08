517d8e059dcd279a2c478fb8a7c3d5c7a669eef3
Babayev2326254 <f.babayev1389@gmail.com>
2024-11-09 02:56:24 +0400

    Task complited

--- src/main/java/learning/maxnumber.java
+++ src/main/java/learning/maxnumber.java
@@ -2,8 +2,10 @@ package learning;
 
 public class maxnumber {
     public static void main(String[] args) {
+
         int[] number = {5, 12, 3, 19, 7};
         int max = number[0];
+
         for (int i = 1; i < number.length; i++) {
             if (max < number[i]) {
                 max = number[i];
