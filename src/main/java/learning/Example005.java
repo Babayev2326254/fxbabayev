4e06d287d525c0d72b5437c655dca9d9a2390b86
Babayev2326254 <f.babayev1389@gmail.com>
2024-11-09 05:21:43 +0400

    first edition

---
+++ src/main/java/learning/Example005.java
@@ -0,0 +1,22 @@
+package learning;
+
+import java.util.Scanner;
+
+public class Example005 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name : ");
+        String word = scanner.nextLine();
+        int a = word.length();
+        String[] keuwords = {"F","B","X"};
+
+        for (int i = 0; i < a; i++) {
+            if(word.charAt(i) == keuwords[0].charAt(0) || word.charAt(i) == keuwords[1].charAt(0) || word.charAt(i) == keuwords[2].charAt(0)) {
+                word = word.replace(keuwords[i], "?");
+                continue;
+            }
+        }
+        System.out.println(word);
+
+    }
+}
